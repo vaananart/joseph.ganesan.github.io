6244f464da0659bec086134df0111c71ca5e3a0d
Joseph Ganesan <vaananart@gmail.com>
2021-12-19 19:15:54 +1100

    deleted all setting and created a new project in a folder

--- resumeInAngular/karma.conf.js
+++ resumeInAngular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/onlineResume'),
+      dir: require('path').join(__dirname, './coverage/resumeInAngular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
